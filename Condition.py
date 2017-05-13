978ec566d8e586d9011460c3c7beb557c57b80cd
nnvnaveen <naveen.085@gmail.com>
2017-05-12 21:40:44 -0400

    Create Condition.py

---
+++ Condition.py
@@ -0,0 +1,7 @@
+# Class to create conditional examples of python samples to be used
+
+a=123
+if isinstance(a, str):
+    print "Its is a String value"
+else:
+    print "false it is not a string values"
