7045d7caf11e32450d21c53001a9cf5deecd25a6
nnvnaveen <naveen.085@gmail.com>
2017-05-12 21:41:21 -0400

    Create StringManipulation.py

---
+++ StringManipulation.py
@@ -0,0 +1,26 @@
+#StringManipulation.py
+
+    # Script to manipulate string values and
+
+    str = 'nagavenkata navlam'
+    print(str)
+    print(str[0])
+    print "substring"+str[2:]
+    print str*2
+    print str[2:9]
+
+
+    #list manipulation
+    list = ['abcd',786, 2.23, 'john']
+    #print the list details
+    print list
+    print list[0]
+    print list[1:3]
+    print list[2:]
+    print 'List Data', list
+
+    # tuple implementation
+    dataTuple = ('tupl1', 1,3.4, "4.3", 5)
+    print "Tupe data",dataTuple
+    print 'updating tuple'
+    dataTuple[2]=100
